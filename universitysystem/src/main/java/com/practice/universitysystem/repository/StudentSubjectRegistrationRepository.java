9109204b72ab660ca4ad74a6030d06b3a08b26fa
Keneth <keneth.berriom@perficient.com>
2022-07-29 10:31:02 -0500

    Refactor studentSubject model package name

--- universitysystem/src/main/java/com/practice/universitysystem/repository/StudentSubjectRegistrationRepository.java
+++ universitysystem/src/main/java/com/practice/universitysystem/repository/StudentSubjectRegistrationRepository.java
@@ -1,7 +1,7 @@
 package com.practice.universitysystem.repository;
 
-import com.practice.universitysystem.model.studentsubject.StudentSubjectRegistration;
-import com.practice.universitysystem.model.studentsubject.StudentSubjectRegistrationId;
+import com.practice.universitysystem.model.student_subject.StudentSubjectRegistration;
+import com.practice.universitysystem.model.student_subject.StudentSubjectRegistrationId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentSubjectRegistrationRepository extends JpaRepository<StudentSubjectRegistration, StudentSubjectRegistrationId> {
