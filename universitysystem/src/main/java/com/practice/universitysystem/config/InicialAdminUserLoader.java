d5644bb6b3a25d3b6d2136dcf08ff924ecaaf2d8
Keneth <keneth.berriom@perficient.com>
2022-08-09 16:12:21 -0500

    Refactor take admin init info from application.properties

--- universitysystem/src/main/java/com/practice/universitysystem/config/InicialAdminUserLoader.java
+++ universitysystem/src/main/java/com/practice/universitysystem/config/InicialAdminUserLoader.java
@@ -4,6 +4,7 @@ import com.practice.universitysystem.model.users.admin.Admin;
 import com.practice.universitysystem.repository.users.admin.AdminRepository;
 import com.practice.universitysystem.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -18,8 +19,14 @@ public class InicialAdminUserLoader implements ApplicationRunner {
     @Autowired
     AuthService authService;
 
+    @Value("${admin_name}")
+    private String adminName;
+    @Value("${admin_password}")
+    private String password;
+    @Value("${admin_mail_domain}")
+    private String domain;
+
     public void run(ApplicationArguments args) {
-        String adminName = "admin";
         if (adminRepository.count() != 0) {
             return;
         }
@@ -27,12 +34,12 @@ public class InicialAdminUserLoader implements ApplicationRunner {
         admin.setName(adminName);
         admin.setLastName(adminName);
         admin.setGovernmentId("0");
-        admin.setEmail(adminName + "@universitySystem.com");
+        admin.setEmail(adminName + domain);
         admin.setMobilePhone("0");
         admin.setLandPhone("0");
         admin.setBirthdate(new Date());
         admin.setUsername(adminName);
-        admin.setUserPassword(authService.getEncodedPassword("admin"));
+        admin.setUserPassword(authService.getEncodedPassword(password));
         admin.setEnrollmentDate(new Date());
         adminRepository.save(admin);
     }
