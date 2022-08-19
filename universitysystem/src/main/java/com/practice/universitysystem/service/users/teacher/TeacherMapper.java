fa09bf198a0da7a9c113ac0f68aec1a305f38841
Keneth <45386437+kberriom@users.noreply.github.com>
2022-08-19 17:15:12 -0500

    Update TeacherMapper.java

--- universitysystem/src/main/java/com/practice/universitysystem/service/users/teacher/TeacherMapper.java
+++ universitysystem/src/main/java/com/practice/universitysystem/service/users/teacher/TeacherMapper.java
@@ -3,7 +3,12 @@ package com.practice.universitysystem.service.users.teacher;
 import com.practice.universitysystem.dto.users.TeacherDto;
 import com.practice.universitysystem.model.users.teacher.Teacher;
 import com.practice.universitysystem.service.users.UserMapper;
-import org.mapstruct.*;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper
 public interface TeacherMapper extends UserMapper<Teacher, TeacherDto> {
